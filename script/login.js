6cf7f3e0c16e29c3e290628922b76589bd505f82
kyzentyven <kyzentyven@gmail.com>
2025-04-27 05:20:07 +0600

    Implemented the validation of login credentials.

--- script/login.js
+++ script/login.js
@@ -1,4 +1,20 @@
 document.getElementById("login-btn").addEventListener("click",
 function(event){
     event.preventDefault();
+    const accountNumber = document.getElementById("account-number").
+    value;
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+
+    if(accountNumber.length === 11){
+        if(convertedPin === 1234){
+            console.log('Correct PIN');
+        }
+        else{
+            console.log('Wrong PIN');
+        }
+    }
+    else{
+        console.log('Need valid account number');
+    }
 })
