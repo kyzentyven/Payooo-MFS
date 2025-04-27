d4e70576d87404f926be1dfcd4e77f30e830574c
kyzentyven <kyzentyven@gmail.com>
2025-04-27 15:24:19 +0600

    Added the fuctionality for add money.

---
+++ script/addMoney.js
@@ -0,0 +1,18 @@
+document.getElementById('add-money-btn').addEventListener('click',
+function(event){
+    event.preventDefault();
+    const amount = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amount);
+    const pin = document.getElementById('pin').value;
+    const convertedPin = parseInt(pin);
+    const mainBalance = document.getElementById('main-balance').innerText;
+    const convertedMainBalance = parseFloat(mainBalance);
+
+    if(convertedPin === 1234){
+        const sum = convertedMainBalance + convertedAmount;
+        document.getElementById('main-balance').innerText = sum;
+    }
+    else{
+        alert('Wrong PIN');
+    }
+})
